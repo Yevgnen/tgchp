259dc52cbf1f97d955ab2cdd6218609016a692e0
Yevgnen Koh <wherejoystarts@gmail.com>
2024-04-04 13:56:13 +0800

    Add convenient functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
+use std::io::Read;
+
+use serde_json::Result;
+
 mod utils;
 
 pub mod types;
 pub use types::*;
+
+pub fn from_str(s: &str) -> Result<ChatHistory> {
+    serde_json::from_str(s)
+}
+
+pub fn from_reader<R>(reader: R) -> Result<ChatHistory>
+where
+    R: Read,
+{
+    serde_json::from_reader(reader)
+}
